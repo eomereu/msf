497a6e1a5e299f0ecb6247e763f01c61e4369f64
Ersegun Omer EROL <eomereu@gmail.com>
2020-09-10 08:14:26 +0200

    MSF v1.1

--- P01_War/script.js
+++ P01_War/script.js
@@ -1,50 +1,62 @@
 var guardiansTeamName = document.getElementById("guardiansTeamName");
 var technervaTeamName = document.getElementById("technervaTeamName");
 var bktTeamName = document.getElementById("bktTeamName");
+var exception = false;
+reEventAdditionToGTB(); //adds click listener to gtb (guardins/technerva/bkt) team names
 
-technervaTeamName.addEventListener("click", function(){
-  document.querySelector("#guardiansSlot1").innerHTML = "<img src=\"../imgs/toon_pngs/vision.png\" class=\"resizable\" id=\"vision\" alt=\"vision\">";
-  document.querySelector("#guardiansSlot2").innerHTML = "<img src=\"../imgs/toon_pngs/minn_erva.png\" class=\"resizable\" id=\"minn_erva\" alt=\"minn_erva\">";
-  this.style.color = "rgb(15, 230, 7)";
-  guardiansTeamName.style.color = "white";
-  bktTeamName.style.color = "white";
-});
-
-var aimtronVs = document.querySelector("#aimtronVsSelect");
-aimtronVs.addEventListener("change", function(){
-  document.querySelector("#aimtronVsTeamName").textContent = this.value;
-  document.querySelector("#aimtronVsTeamName").style = "text-transform: uppercase;";
-});
-
-var xforceVs = document.querySelector("#xforceVsSelect");
-xforceVs.addEventListener("change", function(){
-  document.querySelector("#xforceVsTeamName").textContent = this.value;
-  document.querySelector("#xforceVsTeamName").style = "text-transform: uppercase;";
-  if(this.value === "em-marauders") { document.querySelector("#xforceVsExp").textContent = "Disrupt Stryfe first" }
-  else if(this.value === "asgardians") { document.querySelector("#xforceVsExp").textContent = "Kill them all" }
-  else if(this.value === "mercs") { document.querySelector("#xforceVsExp").textContent = "Mercs are easy" }
-  else { document.querySelector("#xforceVsExp").textContent = "Black order is so strong" }
-});
-
-
-
-
-
-
-
-/*
-var vsSelections = document.getElementsByTagName("select");
-console.log(vsSelections);
-var i;
-for( i = 0; i < vsSelections.length; i++){
-  console.log(vsSelections[i]);
-  vsSelections[i].addEventListener("change", function(){
-    console.log(vsSelections.item(i));
-    var tempName = "#" + vsSelections[i].getAttribute("id") + "TeamName";
-    var tempExp = "#" + vsSelections[i].getAttribute("id") + "Exp";
-    console.log(tempName + " " + tempExp);
-    document.querySelector(tempName).textContent = this.value;
-    document.querySelector(tempName).style = "text-transform: uppercase;";
-    document.querySelector(tempExp).textContent = "Lorem ipsum...";
+var selects = document.querySelectorAll("select");
+for( var i = 0; i < selects.length; i++){
+  // Adding event listener on all Selects to change the displayed team name and the teams
+  selects[i].addEventListener("change", function(){
+    console.log(this.id);
+    var tempName = "#" + this.id + "TeamName";
+    console.log(tempName);
+    if(this.id === "pa20Vs") {
+      exception = true;
+      console.log(exception);
+    }
+    if(this.value === "gtb") {
+      document.querySelector(tempName).innerHTML = "<span style=\"color: rgb(15, 230, 7);\" id=\"guardiansTeamName\">GUARDIANS </span>/<span id=\"technervaTeamName\"> TECHNERVA </span>/<span id=\"bktTeamName\"> BKT </span>";
+      document.getElementById("gtbExp").style.display = "block";
+      guardiansTeamName = document.getElementById("guardiansTeamName");
+      technervaTeamName = document.getElementById("technervaTeamName");
+      bktTeamName = document.getElementById("bktTeamName");
+      reEventAdditionToGTB();
+    } else if (exception === true && this.value !== "gtb") {
+      document.getElementById("gtbExp").style.display = "none";
+      document.querySelector(tempName).textContent = this.value;
+      document.querySelector(tempName).style = "text-transform: uppercase;";
+    }
+    exception = false;
   });
-};*/
+}
+
+function reEventAdditionToGTB(){
+  technervaTeamName.addEventListener("click", function(){
+    document.querySelectorAll(".guardiansSlot1")[0].innerHTML = "<img src=\"../imgs/toon_pngs/vision.png\" class=\"resizable\" id=\"vision\" alt=\"vision\">";
+    document.querySelectorAll(".guardiansSlot1")[1].innerHTML = "<img src=\"../imgs/toon_pngs/vision.png\" class=\"resizable\" id=\"vision\" alt=\"vision\">";
+    document.querySelectorAll(".guardiansSlot2")[0].innerHTML = "<img src=\"../imgs/toon_pngs/minn_erva.png\" class=\"resizable\" id=\"minn_erva\" alt=\"minn_erva\">";
+    document.querySelectorAll(".guardiansSlot2")[1].innerHTML = "<img src=\"../imgs/toon_pngs/minn_erva.png\" class=\"resizable\" id=\"minn_erva\" alt=\"minn_erva\">";
+    this.style.color = "rgb(15, 230, 7)";
+    guardiansTeamName.style.color = "white";
+    bktTeamName.style.color = "white";
+  });
+    bktTeamName.addEventListener("click", function(){
+    document.querySelectorAll(".guardiansSlot1")[0].innerHTML = "<img src=\"../imgs/toon_pngs/minn_erva.png\" class=\"resizable\" id=\"minn_erva\" alt=\"minn_erva\">";
+    document.querySelectorAll(".guardiansSlot1")[1].innerHTML = "<img src=\"../imgs/toon_pngs/minn_erva.png\" class=\"resizable\" id=\"minn_erva\" alt=\"minn_erva\">";
+    document.querySelectorAll(".guardiansSlot2")[0].innerHTML = "<img src=\"../imgs/toon_pngs/thanos.png\" class=\"resizable\" id=\"thanos\" alt=\"thanos\">";
+    document.querySelectorAll(".guardiansSlot2")[1].innerHTML = "<img src=\"../imgs/toon_pngs/thanos.png\" class=\"resizable\" id=\"thanos\" alt=\"thanos\">";
+    this.style.color = "rgb(15, 230, 7)";
+    technervaTeamName.style.color = "white";
+    guardiansTeamName.style.color = "white";
+  });
+    guardiansTeamName.addEventListener("click", function(){
+    document.querySelectorAll(".guardiansSlot1")[0].innerHTML = "<img src=\"../imgs/toon_pngs/drax.png\" class=\"resizable\" id=\"drax\" alt=\"drax\">";
+    document.querySelectorAll(".guardiansSlot1")[1].innerHTML = "<img src=\"../imgs/toon_pngs/drax.png\" class=\"resizable\" id=\"drax\" alt=\"drax\">";
+    document.querySelectorAll(".guardiansSlot2")[0].innerHTML = "<img src=\"../imgs/toon_pngs/mantis.png\" class=\"resizable\" id=\"mantis\" alt=\"mantis\">";
+    document.querySelectorAll(".guardiansSlot2")[1].innerHTML = "<img src=\"../imgs/toon_pngs/mantis.png\" class=\"resizable\" id=\"mantis\" alt=\"mantis\">";
+    this.style.color = "rgb(15, 230, 7)";
+    technervaTeamName.style.color = "white";
+    bktTeamName.style.color = "white";
+  });
+}
