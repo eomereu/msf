269d4bce8d482161c7c79b85da0d22fd8d3c9436
Ersegun Omer EROL <eomereu@gmail.com>
2020-09-10 02:20:10 +0200

    MSF v1.0

--- P01_War/script.js
+++ P01_War/script.js
@@ -9,3 +9,42 @@ technervaTeamName.addEventListener("click", function(){
   guardiansTeamName.style.color = "white";
   bktTeamName.style.color = "white";
 });
+
+var aimtronVs = document.querySelector("#aimtronVsSelect");
+aimtronVs.addEventListener("change", function(){
+  document.querySelector("#aimtronVsTeamName").textContent = this.value;
+  document.querySelector("#aimtronVsTeamName").style = "text-transform: uppercase;";
+});
+
+var xforceVs = document.querySelector("#xforceVsSelect");
+xforceVs.addEventListener("change", function(){
+  document.querySelector("#xforceVsTeamName").textContent = this.value;
+  document.querySelector("#xforceVsTeamName").style = "text-transform: uppercase;";
+  if(this.value === "em-marauders") { document.querySelector("#xforceVsExp").textContent = "Disrupt Stryfe first" }
+  else if(this.value === "asgardians") { document.querySelector("#xforceVsExp").textContent = "Kill them all" }
+  else if(this.value === "mercs") { document.querySelector("#xforceVsExp").textContent = "Mercs are easy" }
+  else { document.querySelector("#xforceVsExp").textContent = "Black order is so strong" }
+});
+
+
+
+
+
+
+
+/*
+var vsSelections = document.getElementsByTagName("select");
+console.log(vsSelections);
+var i;
+for( i = 0; i < vsSelections.length; i++){
+  console.log(vsSelections[i]);
+  vsSelections[i].addEventListener("change", function(){
+    console.log(vsSelections.item(i));
+    var tempName = "#" + vsSelections[i].getAttribute("id") + "TeamName";
+    var tempExp = "#" + vsSelections[i].getAttribute("id") + "Exp";
+    console.log(tempName + " " + tempExp);
+    document.querySelector(tempName).textContent = this.value;
+    document.querySelector(tempName).style = "text-transform: uppercase;";
+    document.querySelector(tempExp).textContent = "Lorem ipsum...";
+  });
+};*/
